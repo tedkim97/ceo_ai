23fef91d469dc3689a59632fcaabc32afe8d7891
Ted Kim <14024487+tedkim97@users.noreply.github.com>
2024-11-23 16:18:25 -0500

    Initial commit with first draft

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+## TODO
+Pop up window
+Response loading animation
+NAV bar that lets you pick your CEO
---
+++ script.js
@@ -0,0 +1,8 @@
+// wait_timer
+
+const jokes = [
+	"Layoff half your work force",
+	"Increase your executive compensation",
+	"Add AI to your product",
+];
+console.log(jokes)
